4fa6eeb3e6a727972f2456f4728869d0ca6c4454
Danny Lin <logicmelody@gmail.com>
2019-04-14 15:49:09 +0800

    Setup for Redux

--- src/components/clock/index.js
+++ src/components/clock/index.js
@@ -1,3 +1,41 @@
-import Clock from './Clock';
+import React, { Component } from 'react';
+import { Text, View } from 'react-native';
+
+import styles from './styles';
+
+class Clock extends Component {
+	constructor(props) {
+		super(props);
+		this.state = { date: new Date() };
+	}
+
+	componentDidMount() {
+		this.timerID = setInterval(
+			() => this.tick(),
+			1000
+		);
+	}
+
+	componentWillUnmount() {
+		clearInterval(this.timerID);
+	}
+
+	tick() {
+		this.setState({
+			date: new Date()
+		});
+	}
+
+	render() {
+		console.log('Clock render');
+
+		return (
+			<View>
+				<Text>Hello, world!</Text>
+				<Text>It is {this.state.date.toLocaleTimeString()}.</Text>
+			</View>
+		);
+	}
+}
 
 export default Clock;
