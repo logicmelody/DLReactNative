166c2fa3867592e567e90dea13e48fea1751ec0d
Danny Lin <danny.l@ljit.io>
2019-05-24 18:13:55 +0800

    RepoItem

---
+++ src/components/repo-item/styles.js
@@ -0,0 +1,23 @@
+import {
+	StyleSheet,
+} from 'react-native';
+
+const styles = StyleSheet.create({
+	item: {
+		paddingVertical: 8,
+		paddingHorizontal: 24,
+	},
+	selected: {
+		backgroundColor: '#B2DFDB',
+	},
+	stars: {
+		paddingBottom: 8,
+	},
+	title: {
+		fontSize: 16,
+		fontWeight: 'bold',
+		paddingBottom: 4,
+	},
+});
+
+export default styles;
