d0782fa73a007058a0d278a9f1bfd6ec160e2658
Danny Lin <logicmelody@gmail.com>
2019-06-09 10:53:46 +0800

    Styling the bottom tab on Android platform

--- src/app.js
+++ src/app.js
@@ -1,3 +1,6 @@
+import {
+	Platform,
+} from 'react-native';
 import { createStore } from 'redux';
 import { Provider } from 'react-redux';
 import { Navigation } from 'react-native-navigation';
@@ -6,34 +9,52 @@ import ScreenCatalog from './screens/screen-catalog';
 import reducers from './reducers';
 import registerScreens from './register-screens';
 
+const tabs = [
+	{
+		label: '1',
+		screen: ScreenCatalog.FIRST_SCREEN, // this is a registered name for a screen
+		icon: require('./img/first.png'),
+		selectedIcon: require('./img/first-selected.png'), // iOS only
+		title: 'Screen 1',
+	},
+	{
+		label: '2',
+		screen: ScreenCatalog.SECOND_SCREEN,
+		icon: require('./img/second.png'),
+		selectedIcon: require('./img/second-selected.png'), // iOS only
+		title: 'Screen 2',
+	}
+];
+
+const tabsStyle = {
+	tabBarButtonColor: 'white',
+	tabBarSelectedButtonColor: '#ffa200',
+	tabBarBackgroundColor: '#551A8B',
+	forceTitlesDisplay: true,
+};
+
+const appStyle = {
+	tabBarButtonColor: 'white',
+	tabBarSelectedButtonColor: '#ffa200',
+	tabBarBackgroundColor: '#551A8B',
+	forceTitlesDisplay: true,
+};
+
 const store = createStore(reducers, {});
 
 registerScreens(store, Provider);
 startApp();
 
 function startApp() {
-	Navigation.startTabBasedApp({
-		tabs: [
-			{
-				label: '1',
-				screen: ScreenCatalog.FIRST_SCREEN, // this is a registered name for a screen
-				// icon: require('./img/first.png'),
-				// selectedIcon: require('./img/first-selected.png'), // iOS only
-				title: 'Screen 1',
-			},
-			{
-				label: '2',
-				screen: ScreenCatalog.SECOND_SCREEN,
-				// icon: require('./img/second.png'),
-				// selectedIcon: require('./img/second-selected.png'), // iOS only
-				title: 'Screen 2',
-			}
-		],
-		tabsStyle: {
-			tabBarButtonColor: '#ff0000',
-			tabBarSelectedButtonColor: '#ffa200',
-			tabBarBackgroundColor: '#551A8B',
-			forceTitlesDisplay: true,
-		}
-	});
+	const attributes = Platform.OS === "ios" ?
+		{
+			tabs,
+			tabsStyle,
+		} :
+		{
+			tabs,
+			appStyle,
+		};
+
+	Navigation.startTabBasedApp(attributes);
 }
